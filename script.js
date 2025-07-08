2d17669dc4716aaf4ef5cbaec00c2905e7bbee6b
marcelambicalho <mbicalho821@gmail.com>
2025-07-08 13:48:21 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
-// Wait until the DOM is fully loaded
 document.addEventListener('DOMContentLoaded', function() {
   // Get the buttons
   const hobbiesBtn = document.querySelector('.hobbies-button');
   const interestsBtn = document.querySelector('.interests-button');
 
-  // Toggle function for hobbies
   hobbiesBtn.addEventListener('click', function() {
     const hobParagraphs = document.querySelectorAll('.hob-p');
     hobParagraphs.forEach(p => {
@@ -16,7 +14,6 @@ document.addEventListener('DOMContentLoaded', function() {
     });
   });
 
-  // Toggle function for interests
   interestsBtn.addEventListener('click', function() {
     const intParagraphs = document.querySelectorAll('.int-p');
     intParagraphs.forEach(p => {
